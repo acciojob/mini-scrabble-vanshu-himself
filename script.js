6ed0fb0e1ed6434dd33c6c729d9604bfe06857bb
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-21 11:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const textbar=document.getElementById('evaluatedText');
+// textbar.addEventListener('in')
+// function counter(){
+// 	counter.innerHTML=textbar.value.length;
+// })
+ textbar.addEventListener("input", function countWord() {
+          
+            counter.innerText = textbar.value.length;
+          });
+
